0740d0e99c801835aa1fab31c7b2d0988ac19d13
Andrew Pyle <andy.pyle@gmail.com>
2015-05-01 19:41:04 -0700

    Added controller, gender preference, and a clear button. Made class check in to function.

---
+++ js/app.js
@@ -0,0 +1,50 @@
+angular.module('madlibApp', [])
+	.controller('madlibCtrl', function($scope){
+		
+		$scope.name = '';
+		$scope.jobTitle = '';
+		$scope.tediousTask = '';
+		$scope.dirtyTask = '';
+		$scope.celebrity = '';
+		$scope.uselessSkill = '';
+		$scope.adjective = '';
+		$scope.obnoxious = '';
+		$scope.hugeNumber = '';
+
+		$scope.genderIs = 'he';
+		$scope.genderOwner = 'him';
+		$scope.genderOwner2 = 'his';
+
+		$scope.checkGender = function(gender){
+			gender = $scope.gender;
+			if(!gender){
+				$scope.genderIs = 'he';
+				$scope.genderOwner = 'him';
+				$scope.genderOwner2 = 'his';
+			} else {
+				$scope.genderIs = 'she';
+				$scope.genderOwner = 'her';
+				$scope.genderOwner2 = 'her';
+			}		
+		}
+
+		$scope.checkClass = function(word){
+			return !word ? 'blankSpace':'';
+		}
+
+		$scope.clearFields = function(){
+			$scope.name = '';
+			$scope.jobTitle = '';
+			$scope.tediousTask = '';
+			$scope.dirtyTask = '';
+			$scope.celebrity = '';
+			$scope.uselessSkill = '';
+			$scope.adjective = '';
+			$scope.obnoxious = '';
+			$scope.hugeNumber = '';
+
+			$scope.genderIs = 'he';
+			$scope.genderOwner = 'him';
+			$scope.genderOwner2 = 'his';
+		};
+	});
